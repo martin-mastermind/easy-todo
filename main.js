4ee0589a4cdd38f0e113f0e6671235f40dd1bf76
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2023-08-07 13:45:15 +0300

    Optimizing + reworking js

--- main.js
+++ main.js
@@ -1,22 +1,53 @@
+const CARD_CLASS = 'card'
+const CARD_SOLVED_CLASS = 'card card-solved'
+
+const CLOSE_ICON_DATA = {
+    className: 'card__close',
+    html: '✘'
+}
+const SOLVE_ICON_DATA = {
+    className: 'card__solve',
+    html: '✓'
+}
+
+const ARR_LIMiT = 32
+const MESSAGES = {
+    errorType: 'Invalid value type.',
+    maxLengthExceeded: 'Cards array exceeded length limit (max 32 items).'
+}
+
 const titleInput = document.querySelector('#title')
 const descriptionTextarea = document.querySelector('#description')
 const addButton = document.querySelector('button')
 const cards = document.querySelector('.cards')
 
-let cardsArr;
+const cardsArr = {
+    value: [],
+    set(newValue) {
+        if (!Array.isArray(newValue)) {
+            alert(MESSAGES.errorType)
+            return false
+        }
+
+        addButton.disabled = newValue.length === ARR_LIMiT
+
+        if (newValue.length > ARR_LIMiT) {
+            alert(MESSAGES.maxLengthExceeded)
+            return false
+        }
+
+        this.value = newValue
+        localStorage.cards = JSON.stringify(this.value)
+
+        drawCards()
+        return true
+    }
+}
 
 try {
-    cardsArr = JSON.parse(localStorage.cards)
-
-    // Проверка на массив
-    if (!cardsArr.length) {
-        cardsArr = []
-        localStorage.cards = ''
-    }
-
-    drawCards()
+    if (localStorage.cards && !cardsArr.set(JSON.parse(localStorage.cards))) throw null
 } catch {
-    cardsArr = []
+    localStorage.cards = ''
 }
 
 function createCard(i, cardObj) {
@@ -24,11 +55,7 @@ function createCard(i, cardObj) {
 
     const card = document.createElement('article')
 
-    if (cardObj.solved) {
-        card.className = 'card card-solved' 
-    } else {
-        card.className = 'card'
-    }
+    card.className = cardObj.solved ? CARD_SOLVED_CLASS : CARD_CLASS
 
     const h2 = document.createElement('h2')
     const p = document.createElement('p')
@@ -36,33 +63,31 @@ function createCard(i, cardObj) {
 
     h2.innerHTML = cardObj.title
     p.innerHTML = cardObj.description
-    small.innerHTML = now.toLocaleDateString() + " " + now.toLocaleTimeString()
+    small.innerHTML = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`
 
     const icons = document.createElement('div')
     
     const closeIcon = document.createElement('i')
     const solveIcon = document.createElement('i')
 
-    closeIcon.className = 'card__close'
-    solveIcon.className = 'card__solve'
+    closeIcon.className = CLOSE_ICON_DATA.className
+    solveIcon.className = SOLVE_ICON_DATA.className
 
-    closeIcon.innerHTML = '✘'
-    solveIcon.innerHTML = '✓'
+    closeIcon.innerHTML = CLOSE_ICON_DATA.html
+    solveIcon.innerHTML = SOLVE_ICON_DATA.html
 
     closeIcon.addEventListener('click', () => {
-        cardsArr.splice(i, 1)
+        const cardsTemp = [...cardsArr.value]
+        cardsTemp.splice(i, 1)
 
-        localStorage.cards = JSON.stringify(cardsArr)
-        drawCards()
+        cardsArr.set(cardsTemp)
     })
 
     solveIcon.addEventListener('click', () => {
-        const cardTemp = cardsArr.splice(i, 1)[0]
+        const cardTemp = cardsArr.value.splice(i, 1)[0]
         cardTemp.solved = true
-        cardsArr.push(cardTemp)
 
-        localStorage.cards = JSON.stringify(cardsArr)
-        drawCards()
+        cardsArr.set([...cardsArr.value, cardTemp])
     })
 
     icons.appendChild(closeIcon)
@@ -79,8 +104,8 @@ function createCard(i, cardObj) {
 function drawCards() {
     cards.innerHTML = ''
 
-    for (let i = 0; i < cardsArr.length; i++) {
-        const card = cardsArr[i]
+    for (const i in cardsArr.value) {
+        const card = cardsArr.value[i]
         cards.appendChild(createCard(i, card))
     }
 }
@@ -91,16 +116,14 @@ addButton.addEventListener('click', () => {
 
     if (title.length === 0 || description.length === 0) return
 
-    cardsArr.unshift({
-        title,
-        description,
-        solved: false
-    })
+    cardsArr.set([
+        {
+            title,
+            description,
+            solved: false
+        }, ...cardsArr.value
+    ])
 
     titleInput.value = ''
     descriptionTextarea.value = ''
-
-    localStorage.cards = JSON.stringify(cardsArr)
-
-    drawCards()
 })
